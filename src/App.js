f09997a71e7fc32bb627543a4d164e100bb86f4f
VanshajV <112333275+VanshajV@users.noreply.github.com>
2023-07-30 22:59:28 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -5,8 +5,9 @@ import { useEffect, useState } from 'react';
 import moment from 'moment';
 
 function App() {
-  const date =moment().format("MMMM DD YYYY");
+  const date =moment().format("DD MMMM YYYY");
   const time =moment().format("HH:mm");
+  const day=moment().format('dddd');  
   
   const[data,setData]=useState(null)
   const[city,setcity]=useState("Delhi")
@@ -66,7 +67,7 @@ useEffect(()=>{
           <button onClick={handlesearch} className=' items-center space-x-1 rounded-md flex bg-[#0b121e] w-25 p-2'><p>Search </p><img src="icons8-search-24.png"  className="w-5" alt=""/></button>
         </div>
         <div className=' p-4 rounded-2xl bg-gray-100 md:w-[22rem] pb-5 md:pb-0 mt-6 md:mt-0'> 
-        <div className='text-lg text-black flex justify-between'><span>{time}</span><span>{date}</span></div>
+        <div className='text-lg text-black flex justify-between'><span>{time}</span><div><span className='mr-2'>{day},</span><span>{date}</span></div></div>
         <img className="w-32 mx-auto " src="image_processing20220801-17461-1qbptiv.png" alt="" />
           {!data ? ( <div><h1 className=' text-black text-4xl '>No Data found</h1><p className='text-black'>Please enter city name</p></div>)
          :<>
