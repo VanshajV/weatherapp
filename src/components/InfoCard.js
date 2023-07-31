b6586316058e907b91dc5bc5db178fc098c59334
VanshajV <112333275+VanshajV@users.noreply.github.com>
2023-07-31 10:59:28 +0530

    Add files via upload

--- src/components/InfoCard.js
+++ src/components/InfoCard.js
@@ -6,17 +6,17 @@ export default function InfoCard(props) {
 
       <div className='text-black mt-3 space-y-2'>
         <div className='flex w-24  mx-auto justify-around items-center'><img className='w-6 h-6 mr-1' src="17736.png" alt="" /><h1 className='text-2xl'>{props.title}</h1></div>
-        <div className='flex px-6 justify-center space-x-20'>
-          <span className=' text-left w-40'>Current Temp</span>
-          <p className='w-40'>{props.CurrentTemp} &#176;C</p>
+        <div className='flex px-6 justify-center space-x-10'>
+          <span className='text-left w-40'>Current Temp</span>
+          <p className='w-40 text-right'>{props.CurrentTemp} &#176;C</p>
         </div>
-        <div className='flex  px-6 justify-center space-x-20'>
+        <div className='flex  px-6 justify-center space-x-10'>
           <span className='text-left w-40'>Feels Like</span>
-          <p className='w-40'>{props.feelslike} &#176;C</p>
+          <p className='w-40 text-right'>{props.feelslike} &#176;C</p>
         </div>
-        <div className='flex  px-6 justify-center space-x-20'>
+        <div className='flex  px-6 justify-center space-x-10'>
           <span className=' text-left w-52'>Wind Speed</span>
-          <p className='w-52'>{props.speed} m/s</p>
+          <p className='w-52 text-right'>{props.speed} m/s</p>
         </div>
 
 
