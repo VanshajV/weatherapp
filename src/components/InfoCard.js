1f45b1d210a91b53f627214cba1ec989dc05e7d3
VanshajV <112333275+VanshajV@users.noreply.github.com>
2023-07-31 10:44:10 +0530

    Add files via upload

--- src/components/InfoCard.js
+++ src/components/InfoCard.js
@@ -8,15 +8,15 @@ export default function InfoCard(props) {
         <div className='flex w-24  mx-auto justify-around items-center'><img className='w-6 h-6 mr-1' src="17736.png" alt="" /><h1 className='text-2xl'>{props.title}</h1></div>
         <div className='flex px-6 justify-center space-x-20'>
           <span className='w-40'>CurrentTemp</span>
-          <p className='w-40'>{props.CurrentTemp}&#176; C</p>
+          <p className='w-40'>{props.CurrentTemp} &#176;C</p>
         </div>
         <div className='flex  px-6 justify-center space-x-20'>
           <span className='w-40'>Feels Like</span>
-          <p className='w-40'>{props.feelslike}&#176; C</p>
+          <p className='w-40'>{props.feelslike} &#176;C</p>
         </div>
         <div className='flex  px-6 justify-center space-x-20'>
           <span className='w-52'>Wind Speed</span>
-          <p className='w-52'>{props.speed}m/s</p>
+          <p className='w-52'>{props.speed} m/s</p>
         </div>
 
 
