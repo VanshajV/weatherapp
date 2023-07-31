1163ccd3142e8d276234d08a549595c6f28f8e83
VanshajV <112333275+VanshajV@users.noreply.github.com>
2023-07-31 13:38:03 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -67,7 +67,7 @@ function App() {
       <div className='bg-gradient-to-r h-[100%] from-[#545e68] to-[#293241]'>
         <div className='h-[100vh]'>
           <h1 className='p-10 text-white text-3xl'>WeatherVista</h1>
-          <div className='mx-auto w-[20rem] mt-10 md:w-[40rem]  text-white md:h-96 rounded-3xl p-4 flex flex-col md:flex-row bg-[#0b121e] justify-between '>
+          <div className='mx-auto w-[23rem] mt-10 md:w-[40rem]  text-white md:h-96 rounded-3xl p-4 flex flex-col md:flex-row bg-[#0b121e] justify-between '>
 
             <div className='flex items-center justify-center space-y-4 p-2 flex-col rounded-2xl md:w-[15rem] bg-[#202b3c] pt-5'>
               <input className='px-2 p-2 rounded-md text-black text-center' type="text" placeholder='Enter a city' onChange={changecity} />
